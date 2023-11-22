0ae01bc283a4b17a4b30e1cece6bd7dced529a67
kirtan234 <kirtanashok.patel@mail.utoronto.ca>
2023-11-22 00:14:31 -0500

    Basic POSt Checker for CompSci Major/Spec

--- app/src/main/java/com/example/myfirstapp/POStChecker.java
+++ app/src/main/java/com/example/myfirstapp/POStChecker.java
@@ -1,4 +1,27 @@
 package com.example.myfirstapp;
 
+import java.util.Scanner;
+
 public class POStChecker {
+    public boolean checkEligibility(double a67, double a48, double a22, double a31, double a37) {
+        double gpa = (a67 + a48 + a22 + a31 + a37) / 5;
+        int cMinusCount = 0;
+        if (gpa < 2.5) {
+            return false;
+        }
+        if (a48 < 3.0) {
+            return false;
+        }
+        if (a67 >= 1.7) {
+            cMinusCount++;
+        }
+        if (a22 >= 1.7) {
+            cMinusCount++;
+        }
+        if (a37 >= 1.7) {
+            cMinusCount++;
+        }
+        return (cMinusCount >= 2);
+    }
 }
+
