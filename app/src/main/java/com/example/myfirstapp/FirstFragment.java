7d52f56d129c019b06a0f07c5176a1754d94eca7
Adrit-3001 <adrit.panday@mail.utoronto.ca>
2023-11-21 23:39:44 -0500

    changes to make buttons work

--- app/src/main/java/com/example/myfirstapp/FirstFragment.java
+++ app/src/main/java/com/example/myfirstapp/FirstFragment.java
@@ -11,7 +11,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 import com.example.myfirstapp.databinding.FragmentFirstBinding;
-
+// aaaaaaaaaaaa
 public class FirstFragment extends Fragment {
 
 private FragmentFirstBinding binding;
