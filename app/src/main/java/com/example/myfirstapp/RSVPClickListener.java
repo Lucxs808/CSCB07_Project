a208f1290623b69e0826fb75907e7e3db85ca83f
Adrit-3001 <adrit.panday@mail.utoronto.ca>
2023-12-01 11:35:22 -0500

    random push ignore please

--- app/src/main/java/com/example/myfirstapp/RSVPClickListener.java
+++ app/src/main/java/com/example/myfirstapp/RSVPClickListener.java
@@ -6,27 +6,31 @@ import com.example.myfirstapp.Student;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class RSVPClickListener implements EventAdapter.RSVPClickListener {
-
-    private Student currentStudent;
-    private DatabaseReference eventsReference;
-
-    public RSVPClickListener(Student student, DatabaseReference eventsReference) {
-        this.currentStudent = student;
-        this.eventsReference = eventsReference;
-    }
-
-    @Override
-    public void onRSVPClicked(Event event) {
-        // Handle Firebase update here
-        // Use currentStudent.getUtorID() to get the UtorID of the current student
-        String studentID = currentStudent.getUtorID();
-
-        // Update the Firebase database
-        DatabaseReference eventRef = eventsReference.child(event.getId());
-        event.addRSVP(studentID); // Add the student's UtorID to the rsvpList
-        eventRef.setValue(event); // Update the Firebase database with the updated event
-    }
+//public class RSVPClickListener implements EventAdapter.RSVPClickListener {
+//
+//    private Student currentStudent;
+//    private DatabaseReference eventsReference;
+//
+//    public RSVPClickListener(Student student, DatabaseReference eventsReference) {
+//        this.currentStudent = student;
+//        this.eventsReference = eventsReference;
+//    }
+//
+//    @Override
+//    public void onRSVPClicked(Event event) {
+//        // Handle Firebase update here
+//        // Use currentStudent.getUtorID() to get the UtorID of the current student
+//        String studentID = currentStudent.getUtorID();
+//
+//        // Update the Firebase database
+//        DatabaseReference eventRef = eventsReference.child(event.getId());
+//        event.addRSVP(studentID); // Add the student's UtorID to the rsvpList
+//        eventRef.setValue(event); // Update the Firebase database with the updated event
+//    }
+//}
+// RSVPClickListener.java
+public interface RSVPClickListener {
+    void onRSVPClick(Event event);
 }
 
 
